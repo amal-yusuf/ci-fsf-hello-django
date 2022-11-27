9dc9f5b1b1e92ddd7305de9377e5542ef2c123b2
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-11-27 12:08:00 +0000

    prepare to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 # from pathlib import Path
+from pathlib import Path
 import os
 import dj_database_url
+if os.path.isfile("env.py"):
+    import env
 
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -82,15 +85,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
